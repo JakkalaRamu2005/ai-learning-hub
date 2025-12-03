0abcbfe15003154208bacece501ddd873944e487
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-12-03 11:12:08 +0530

    updated

--- my-app/app/tools/page.tsx
+++ my-app/app/tools/page.tsx
@@ -1,10 +1,27 @@
 "use client"
 import { useState, useEffect } from "react";
+import "./tool.css"
+
+
+interface Tool {
+    name: string,
+    description: string,
+    category: string,
+    link: string,
+    dateAdded: string;
+}
+
+
+
 
 export default function ToolsPage() {
 
-    const [tools, setTools] = useState([]);
+    const [tools, setTools] = useState<Tool[]>([]);
     const [loading, setLoading] = useState(true);
+
+    const [searchText, setSearchText] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
 
     useEffect(() => {
         fetchTools();
@@ -25,6 +42,23 @@ export default function ToolsPage() {
         }
     };
 
+    const categories = ['All', ...new Set(tools.map(t => t.category))];
+
+    const filteredTools = tools.filter(tool => {
+
+        const matchesSearch = tool.name.toLowerCase().includes(searchText.toLocaleLowerCase()) ||
+
+            tool.description.toLowerCase().includes(searchText.toLowerCase());
+
+        const matchesCategory = selectedCategory === 'All' || tool.category === selectedCategory;
+        return matchesSearch && matchesCategory;
+
+    })
+
+
+
+
+
     if (loading) {
         return <div>Loading AI tools...</div>;
     }
@@ -32,8 +66,16 @@ export default function ToolsPage() {
     return (
         <div>
             <h1>AI Tools Directory</h1>
-            <p>Total Tools: {tools.length}</p>
-            <p>Updated Weekly</p>
+
+            <input type="text" placeholder="Search..." value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+
+            <select
+                value={selectedCategory}
+                onChange={(e) => setSelectedCategory(e.target.value)}
+
+            >{categories.map((c) => (
+                <option key={c} value={c}>{c}</option>
+            ))}</select>
 
             {tools.map((tool: any, index: number) => (
                 <div key={index}>
